0e4c5f51e1ed739e7c8f7322f80cb40568b3b2e0
Sourav_Bhattacharya10 <sourav.bhattacharya3@gmail.com>
2024-08-24 21:38:11 +0530

    Updated App Insights Variables.tf

--- azure_app_insight/variables.tf
+++ azure_app_insight/variables.tf
@@ -7,13 +7,3 @@ variable "location" {
   type        = string
   description = "The Azure location where all resources in this example should be created"
 }
-
-variable "osSystem" {
-  type        = string
-  description = "Windows/Linux"
-}
-
-variable "pricingPlan" {
-  type        = string
-  description = "F1/B1/S1/etc."
-}
